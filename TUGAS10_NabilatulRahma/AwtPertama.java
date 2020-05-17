c40d5089b40cacd75ab87a9cf44aa4c89a118f0a
13020180014 <61999864+13020180014@users.noreply.github.com>
2020-05-17 20:44:21 +0800

    Add files via upload

---
+++ TUGAS10_NabilatulRahma/AwtPertama.java
@@ -0,0 +1,15 @@
+/* Nama : Nabilatul Rahma
+   Tgl pengerjaan : 11 mei 2020
+    waktu 22:28
+*/ 
+import java.awt.*;
+public class AwtPertama
+{
+public static void main(String[] args)
+{Frame win=new Frame(" CobaAWT");
+win.setSize(300,200);
+win.show();}
+}
+
+
+    
---
+++ TUGAS10_NabilatulRahma/Contoh1.java
@@ -0,0 +1,13 @@
+/* Nama : Nabilatul Rahma
+   Tgl pengerjaan : 11 mei 2020
+    waktu 22:38
+*/ 
+import javax.swing.*;
+public class Contoh1 {
+public static void main(String[] args) {
+JFrame frame = new JFrame("Contoh Frame");
+frame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
+frame.setSize(400,150);
+frame.show();
+} 
+}
